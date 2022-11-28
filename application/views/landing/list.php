9c61cb383f85cc62102f0a365d4b9b07b38f124c
unknown <ahmadsafrizal01@gmail.com>
2022-11-28 09:52:26 +0800

    first commit

---
+++ application/views/landing/list.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>WRK - Batulicin</title>
+  <meta content="" name="description">
+
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?php echo base_url() ?>assets/assets/img/logowika.png" rel="icon">
+  <link href="<?php echo base_url() ?>assets/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url() ?>assets/assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?php echo base_url() ?>assets/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php echo base_url() ?>assets/assets/css/style.css" rel="stylesheet">
+
+
+<body style="min-height: 100vh;">
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="<?php echo base_url() ?>" class="logo d-flex align-items-center">
+        <img src="<?php echo base_url() ?>assets/assets/img/wrk_logo.png" alt="">
+      </a>
+
+
+
+      <i class="bi bi-list mobile-nav-toggle" style="display: none;"></i>
+      <!-- <a href="<?php echo base_url() ?>" class="btn btn-sm btn-primary ">
+                Kembali
+        </a> -->
+
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center" style="height: 100vh;">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 d-flex flex-column justify-content-center">
+          <h1 data-aos="fade-up">Selamat Datang</h1>
+          <h2 data-aos="fade-up" data-aos-delay="400">Dashboard Sistem Informasi WIKA REKON</h2>
+          <h2 data-aos="fade-up" data-aos-delay="400">Batulicin - Kalimantan Selatan</h2>
+          <div data-aos="fade-up" data-aos-delay="600">
+            <div class="text-lg-start">
+              <br>
+              <a href="<?php echo base_url('login') ?>" class="btn btn-primary ">
+                Login Portal Berita
+              </a>
+              <a href="http://localhost/arsip_wrk/" class=" btn btn-primary ">
+                Login Dashboard
+              </a>
+
+            </div>
+            <br>
+            <a href="<?php echo base_url() ?>" class=" btn btn-primary">
+              Kembali
+            </a>
+
+          </div>
+        </div>
+        <div class="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay="200">
+
+        </div>
+      </div>
+    </div>
+
+
+
+  </section><!-- End Hero -->
+
+  <!-- Vendor JS Files -->
+  <script src="<?php echo base_url() ?>assets/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="<?php echo base_url() ?>assets/assets/vendor/aos/aos.js"></script>
+  <script src="<?php echo base_url() ?>assets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?php echo base_url() ?>assets/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php echo base_url() ?>assets/assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ application/views/layout/head.php
@@ -0,0 +1,78 @@
+<?php
+// Site
+$site_info = $this->konfigurasi_model->listing();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $title; ?></title>
+  <meta name="description" content="<?php echo strip_tags($site_info->tentang) . ', ' . $title ?>">
+  <meta name="keywords" content="<?php echo $site_info->keywords . ', ' . $title  ?>">
+  <meta name="author" content="<?php echo $site_info->namaweb ?>">
+  <!-- icon -->
+  <link rel="shortcut icon" href="<?php echo $this->website->icon(); ?>">
+  <!--[if IE]> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <![endif]-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Plugin css -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/font-awesome.min.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/fonts/flaticon.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/bootstrap.min.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/animate.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/swiper.min.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/lightcase.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/jquery.nstSlider.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/flexslider.css" media="all" />
+  <!-- own style css -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/style.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/rtl.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/tema/assets/css/responsive.css" media="all" />
+  <!-- DataTables CSS -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/admin/plugins/datatables/dataTables.bootstrap4.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/admin/plugins/select2/select2.min.css">
+  <style type="text/css">
+    p {
+      margin-bottom: 15px;
+    }
+
+    .log {
+      display: none !important;
+    }
+
+    @media(max-width: 990px) {
+      .log {
+        display: block !important;
+      }
+
+      .navbar {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      #lightcase-case {
+        width: 90vw;
+      }
+
+      .lightcase-contentInner>img {
+        max-width: 100% !important;
+        max-height: 100% !important;
+      }
+    }
+
+    @media(max-width: 770px) {
+      .navbar {
+        flex-direction: column;
+      }
+
+      .navbar-header,
+      .navbar-collapse {
+        width: 100vw;
+      }
+    }
+  </style>
+</head>
+
+<body>
